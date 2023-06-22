bf24b98fa014fe0aa30754066aef2d96011064ce
Alex Rosenkranz <alex.rosenkranz@datadoghq.com>
2023-06-22 10:32:05 -0400

    updates a few things

--- src/__tests__/components/Header.test.tsx
+++ src/__tests__/components/Header.test.tsx
@@ -19,65 +19,67 @@ jest.mock("next-auth/react", () => ({
   signOut: jest.fn(),
 }));
 
-test("renders the header title", () => {
-  render(<Header />);
+describe("Header", () => {
+  test("renders the header title", () => {
+    render(<Header />);
 
-  const title = screen.getByRole("link", { name: /techstories/i });
-  expect(title).toBeInTheDocument();
-  expect(title).toHaveTextContent("TechStories");
-});
-
-test('renders the "New Post" link', () => {
-  render(<Header />);
-
-  const newPostLink = screen.getByRole("link", { name: /new post/i });
-  expect(newPostLink).toBeInTheDocument();
-  expect(newPostLink).toHaveAttribute("href", "/posts/new");
-});
-
-test("calls the sign-in when no session state", () => {
-  // get session data
-  (useSession as jest.Mock).mockReturnValueOnce({
-    data: null,
+    const title = screen.getByRole("link", { name: /techstories/i });
+    expect(title).toBeInTheDocument();
+    expect(title).toHaveTextContent("TechStories");
   });
-  render(<Header />);
 
-  const signButton = screen.getByRole("button", { name: /sign (in|out)/i });
+  test('renders the "New Post" link', () => {
+    render(<Header />);
 
-  // Test signing in
-  fireEvent.click(signButton);
-  expect(signIn).toHaveBeenCalledTimes(1);
-});
-
-test("calls the sign-out when session state", () => {
-  // get session data
-  (useSession as jest.Mock).mockReturnValueOnce({
-    data: {
-      user: {
-        name: "test",
-        email: "name@example.com",
-        id: "1",
-        image: "https://example.com/image.png",
-      },
-    },
+    const newPostLink = screen.getByRole("link", { name: /new post/i });
+    expect(newPostLink).toBeInTheDocument();
+    expect(newPostLink).toHaveAttribute("href", "/posts/new");
   });
-  render(<Header />);
-  const signButton = screen.getByRole("button", { name: /sign (in|out)/i });
 
-  // Test signing out
-  fireEvent.click(signButton);
-  expect(signOut).toHaveBeenCalledTimes(1);
-});
-
-test("renders the user's name when signed in", () => {
-  // half of the time, clear the session
-  Math.random() < 0.5 &&
+  test("calls the sign-in when no session state", () => {
+    // get session data
     (useSession as jest.Mock).mockReturnValueOnce({
       data: null,
     });
+    render(<Header />);
 
-  render(<Header />);
+    const signButton = screen.getByRole("button", { name: /sign (in|out)/i });
 
-  const name = screen.getByText(/test/i);
-  expect(name).toBeInTheDocument();
+    // Test signing in
+    fireEvent.click(signButton);
+    expect(signIn).toHaveBeenCalledTimes(1);
+  });
+
+  test("calls the sign-out when session state", () => {
+    // get session data
+    (useSession as jest.Mock).mockReturnValueOnce({
+      data: {
+        user: {
+          name: "test",
+          email: "name@example.com",
+          id: "1",
+          image: "https://example.com/image.png",
+        },
+      },
+    });
+    render(<Header />);
+    const signButton = screen.getByRole("button", { name: /sign (in|out)/i });
+
+    // Test signing out
+    fireEvent.click(signButton);
+    expect(signOut).toHaveBeenCalledTimes(1);
+  });
+
+  test("renders the user's name when signed in", () => {
+    // half of the time, clear the session
+    Math.random() < 0.5 &&
+      (useSession as jest.Mock).mockReturnValueOnce({
+        data: null,
+      });
+
+    render(<Header />);
+
+    const name = screen.getByText(/test/i);
+    expect(name).toBeInTheDocument();
+  });
 });
